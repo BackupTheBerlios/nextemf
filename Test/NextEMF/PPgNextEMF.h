068e0ebb4d2adc12315656129c12c64f0e1d0b6b
cyrex2001 <cyrex2001>
2004-11-12 21:41:16 +0000

    Initial revision

---
+++ Test/NextEMF/PPgNextEMF.h
@@ -0,0 +1,57 @@
+//==>Reask sourcen after ip chnage or Quickstart [cyrex2001]
+#if defined (RSAIC) || defined (QUICKSTART) //Reask sourcen after ip chnage or Quickstart
+
+#pragma once
+#include "preferences.h"
+#include "wizard.h"
+#include "TreeOptionsCtrlEx.h"
+
+class CPPgNextEMF : public CPropertyPage
+{
+	DECLARE_DYNAMIC(CPPgNextEMF)
+
+public:
+	CPPgNextEMF();
+	virtual ~CPPgNextEMF();
+
+
+// Dialog Data
+	enum { IDD = IDD_PPG_NEXTEMF };
+
+protected:
+	int m_bIsreaskSourceAfterIPChange;
+	int m_bQuickStart;
+	int m_iQuickStartMaxTime;
+	int m_iQuickStartMaxConnPerFive;
+	int m_iQuickStartMaxConn;
+	int m_bQuickStartAfterIPChange;
+
+	HTREEITEM m_htiIsreaskSourceAfterIPChange;
+	HTREEITEM m_htiQuickStart;
+	HTREEITEM m_htiQuickStartMaxTime;
+	HTREEITEM m_htiQuickStartMaxConnPerFive;
+	HTREEITEM m_htiQuickStartMaxConn;
+	HTREEITEM m_htiQuickStartAfterIPChange;
+
+	CTreeOptionsCtrlEx m_ctrlTreeOptions;
+	bool m_bInitializedTreeOpts;
+
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	afx_msg void OnDestroy();
+	afx_msg LRESULT OnTreeOptsCtrlNotify(WPARAM wParam, LPARAM lParam);
+
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+
+public:
+	void Localize(void);	
+	void LoadSettings(void);
+	virtual BOOL OnApply();
+	virtual BOOL OnInitDialog();
+	virtual BOOL OnKillActive();
+	afx_msg void OnSettingsChange()			{ SetModified(); }
+	afx_msg void OnNMCustomdrawUpSlotsSlider(NMHDR *pNMHDR, LRESULT *pResult);
+};
+#endif //Reask sourcen after ip chnage or Quickstart
+//<==Reask sourcen after ip chnage or Quickstart [cyrex2001]
