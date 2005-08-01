23b2c5cf0361b492255f2d875a4c05f9cae1ad4a
shadow2004 <shadow2004>
2005-08-01 14:26:07 +0000

    v0.6 Final

--- NextEMF/ModVersion.h
+++ NextEMF/ModVersion.h
@@ -19,8 +19,8 @@
 
 #define MOD_VERSION_MJR		0
 #define MOD_VERSION_MIN		6
-#define MOD_VERSION_BUILD   2
+#define MOD_VERSION_BUILD   3
 
-#define	MOD_VERSION			_T("NextEMF b2")
-#define	MOD_VERSION_LONG	_T("NextEMF b2 v")
+#define	MOD_VERSION			_T("NextEMF ")
+#define	MOD_VERSION_LONG	_T("NextEMF v")
 
